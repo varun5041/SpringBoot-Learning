38d1bdcd2561d0889bb11fde5e4dcb24776ad549
varun5041 <varunsoni21102005@gmail.com>
2026-07-22 00:34:04 +0530

    @value annotation

--- springbootLeern/src/main/java/com/varun/springbootLeern/Controllers/HomeController.java
+++ springbootLeern/src/main/java/com/varun/springbootLeern/Controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.varun.springbootLeern.Controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +9,17 @@ import java.util.List;
 
 @RestController
 public class HomeController {
+    @Value("${mykeys.profilepath}")
+    private String ProfilePath;
 
     @RequestMapping("/getNumbers")
     public List<Integer> getNumbers(){
         List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         return numbers;
+    }
+    @RequestMapping("/profile-path")
+    public String getProfilePath(){
+        return this.ProfilePath;
     }
 
 
